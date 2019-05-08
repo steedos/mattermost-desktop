af2c5597e3af8e38f226d9ce5d84a06823d5a309
Jack Zhuang <zhuangjianguo@hotoa.com>
2019-05-08 14:40:02 +0800

    steedos 版

--- resources/linux/create_desktop_file.sh
+++ resources/linux/create_desktop_file.sh
@@ -5,14 +5,14 @@ THIS_PATH=`readlink -f $0`
 cd `dirname ${THIS_PATH}`
 FULL_PATH=`pwd`
 cd ${WORKING_DIR}
-cat <<EOS > Mattermost.desktop
+cat <<EOS > Steedos.desktop
 [Desktop Entry]
-Name=Mattermost
-Comment=Mattermost Desktop application for Linux
-Exec="${FULL_PATH}/mattermost-desktop"
+Name=Steedos
+Comment=Steedos Desktop application for Linux
+Exec="${FULL_PATH}/steedos-desktop"
 Terminal=false
 Type=Application
 Icon=${FULL_PATH}/icon.svg
 Categories=Network;InstantMessaging;
 EOS
-chmod +x Mattermost.desktop
+chmod +x Steedos.desktop
